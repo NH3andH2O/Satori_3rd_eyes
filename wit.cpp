66ca35cd54470dc8d4412dee3aa4826878cafe3f
NH3andH2O <accord_flytrap6x@icloud.com>
2025-04-06 14:44:35 +0800

    數據讀取

--- wit.cpp
+++ wit.cpp
@@ -28,7 +28,7 @@ int8_t wit::wit_init()
 		while (millis() - startTime < initTimeout)					//超時計時器開始計時
 		{
 			/* wit感測器安裝檢測 */
-			if(this->hwSerial->available()) 			//資料讀取
+			if(this->hwSerial->available() >= 2) 		//資料讀取
 			{
 				if(this->hwSerial->read() == 0x55)		//協議頭檢測
 				{
@@ -39,7 +39,131 @@ int8_t wit::wit_init()
 				}
 			}
 		}
-		return WIT_INIT_ERROR; //初始化超時
+		return WIT_INIT_ERROR;	//初始化超時
 	}
 	return SERIAL_INIT_ERROR;	//Serial初始化錯誤
+}
+
+witData wit::wit_get_data()
+{
+	witData result;		//返回數據結構體
+	uint8_t data[60];	//數據緩存
+	uint8_t length;
+	result.angle_status = WIT_READING;
+	result.angular_speed_status = WIT_READING;
+	result.acceleration_status = WIT_READING;
+	result.magnetic_field_status = WIT_READING;
+
+	/* 數據讀取 */
+	u64_t startTime = millis();					//開始時間
+	while(this->hwSerial->available() < 60)		//确保足够的数据
+	{
+		if(millis() - startTime > dataTimeout)	//超時計時器開始計時
+		{
+			result.status = WIT_TIMEOUT;		//無數據
+			length = this->hwSerial->available();
+			uint8_t temp[length];
+			this->hwSerial->readBytes(temp, length);
+			return result;						//返回數據結構體
+		}
+	}
+	this->hwSerial->readBytes(data, 60);	//讀取數據
+
+	/* 數據解析 */
+	for(uint8_t i = 0; i < 50; i++)
+	{
+		if(data[i] == 0x55)			//協議頭檢測
+		{
+			switch (data[i + 1])	//協議類型檢測
+			{
+			case 0x51:				//加速度
+				if(data[i + 10] == (uint8_t)(data[i] + data[i + 1] + data[i + 2] + data[i + 3] + data[i + 4] + data[i + 5] + data[i + 6] + data[i + 7] + data[i + 8] + data[i + 9]))	//校驗和檢測
+				{
+					result.xaacceleration = (int16_t)((int16_t)(data[i + 3] << 8) | data[i + 2]) / (double)32768 * (double)16;	//加速度計算
+					result.yacceleration = (int16_t)((int16_t)(data[i + 5] << 8) | data[i + 4]) / (double)32768 * (double)16;
+					result.zacceleration = (int16_t)((int16_t)(data[i + 7] << 8) | data[i + 6]) / (double)32768 * (double)16;
+					result.temperature = (int16_t)((int16_t)(data[i + 9] << 8) | data[i + 8]) / (double)100;						//溫度計算
+					result.acceleration_status = 0;
+				}
+				else
+				{
+					result.acceleration_status = WIT_VERITY_ERROR;	//數據驗證錯誤
+				}
+				i += 10;	//跳過數據
+				break;
+			case 0x52:				//角速度
+				if(data[i + 10] == (uint8_t)(data[i] + data[i + 1] + data[i + 2] + data[i + 3] + data[i + 4] + data[i + 5] + data[i + 6] + data[i + 7] + data[i + 8] + data[i + 9]))	//校驗和檢測
+				{
+					result.xangular_speed = (int16_t)((int16_t)(data[i + 3] << 8) | data[i + 2]) / (double)32768 * (double)2000;	//角速度計算
+					result.yangular_speed = (int16_t)((int16_t)(data[i + 5] << 8) | data[i + 4]) / (double)32768 * (double)2000;
+					result.zangular_speed = (int16_t)((int16_t)(data[i + 7] << 8) | data[i + 6]) / (double)32768 * (double)2000;
+					result.angular_speed_status = 0;
+				}
+				else
+				{
+					result.angular_speed_status = WIT_VERITY_ERROR;	//數據驗證錯誤
+				}
+				i += 10;	//跳過數據
+				break;
+			case 0x53:				//角度
+				if(data[i + 10] == (uint8_t)(data[i] + data[i + 1] + data[i + 2] + data[i + 3] + data[i + 4] + data[i + 5] + data[i + 6] + data[i + 7] + data[i + 8] + data[i + 9]))	//校驗和檢測
+				{
+					result.xangle = (int16_t)((int16_t)(data[i + 3] << 8) | data[i + 2]) / (double)32768 * (double)180;	//角度計算
+					result.yangle = (int16_t)((int16_t)(data[i + 5] << 8) | data[i + 4]) / (double)32768 * (double)180;
+					result.zangle = (int16_t)((int16_t)(data[i + 7] << 8) | data[i + 6]) / (double)32768 * (double)180;
+					result.angle_status = 0;
+				}
+				else
+				{
+					result.angle_status = WIT_VERITY_ERROR;	//數據驗證錯誤
+				}
+				i += 10;	//跳過數據
+				break;
+			case 0x54:				//磁場強度
+				if(data[i + 10] == (uint8_t)(data[i] + data[i + 1] + data[i + 2] + data[i + 3] + data[i + 4] + data[i + 5] + data[i + 6] + data[i + 7] + data[i + 8] + data[i + 9]))	//校驗和檢測
+				{
+					result.xmagnetic_field = (int16_t)((int16_t)(data[i + 3] << 8) | data[i + 2]) / (double)150;	//磁場強度計算
+					result.ymagnetic_field = (int16_t)((int16_t)(data[i + 5] << 8) | data[i + 4]) / (double)150;
+					result.zmagnetic_field = (int16_t)((int16_t)(data[i + 7] << 8) | data[i + 6]) / (double)150;
+					result.magnetic_field_status = 0;
+				}
+				else
+				{
+					result.magnetic_field_status = WIT_VERITY_ERROR;	//數據驗證錯誤
+				}
+				i += 10;	//跳過數據
+				break;
+			default:
+				break;
+			}
+
+			/* 數據狀態檢測 */
+			if(result.angle_status != WIT_READING && result.angular_speed_status != WIT_READING && result.acceleration_status != WIT_READING && result.magnetic_field_status != WIT_READING)	//所有數據都讀取完成
+			{
+				if(result.angle_status == 0 && result.angular_speed_status == 0 && result.acceleration_status == 0 && result.magnetic_field_status == 0)	//所有數據都正確
+				{
+					result.status = 0;				//數據正常
+				}
+				else
+				{
+					result.status = WIT_DATA_ERROR;	//數據驗證錯誤
+				}
+				length = this->hwSerial->available();
+				uint8_t temp[length];
+				this->hwSerial->readBytes(temp, length);		//清空Serial緩存
+				return result;						//返回數據結構體
+			}
+		}
+	}
+	length = this->hwSerial->available();
+	uint8_t temp[length];
+	this->hwSerial->readBytes(temp, length);
+	result.status = WIT_UNKNOWN_ERROR;	//數據錯誤
+	return result;						//返回數據結構體
+}
+
+void wit::wit_flush()
+{
+	uint8_t temp[128];
+	this->hwSerial->readBytes(temp, 128);	//讀取數據
 }
--- wit.h
+++ wit.h
@@ -8,16 +8,49 @@
 #define SERIAL_INIT_ERROR 1
 #define WIT_INIT_ERROR 2
 
+/* 數據結構錯誤代碼 */
+#define WIT_NO_DATA 1	//無數據
+#define WIT_TIMEOUT 2	//超時
+#define WIT_DATA_ERROR 3	//數據錯誤
+#define WIT_UNKNOWN_ERROR 4	//未知錯誤
+
+#define WIT_READING 1	//讀取中
+#define WIT_VERITY_ERROR 2	//驗證錯誤
+
 #include <Arduino.h>
+
+typedef struct 
+{
+	uint8_t status = WIT_NO_DATA;	//狀態
+	double xangle;					//角度
+	double yangle;
+	double zangle;
+	uint8_t angle_status;
+	double xangular_speed;			//角速度
+	double yangular_speed;
+	double zangular_speed;
+	uint8_t angular_speed_status;
+	double xaacceleration;			//加速度
+	double yacceleration;
+	double zacceleration;
+	uint8_t acceleration_status;
+	double xmagnetic_field;			//磁場強度
+	double ymagnetic_field;
+	double zmagnetic_field;
+	uint8_t magnetic_field_status;
+	double temperature;				//溫度
+} witData;
 
 class wit
 {
 private:
 
-	/* 常量變數 */
-	const uint8_t WIT_UNLOCK[5] = {0XFF, 0XAA, 0X69, 0X88, 0XB5,};
+	/* 協議 */
+	const uint8_t WIT_UNLOCK[5] = {0XFF, 0XAA, 0X69, 0X88, 0XB5,};	//解鎖協議
 
-	uint16_t initTimeout = 10000;	//初始化超時時間
+	/* 計時器 */
+	const uint16_t initTimeout = 10000;	//初始化超時時間
+	const uint16_t dataTimeout = 10;	//數據超時時間
 
 	/* 參數變數 */
 	uint8_t serialPort;
@@ -27,11 +60,13 @@ private:
 
 	/* Serial端口指標 */
 	HardwareSerial* hwSerial = NULL;
-	
+
 public:
 
 	wit(uint8_t serialPort, uint8_t rxPin, uint8_t txPin, uint32_t baudRate);
 	int8_t wit_init();
+	witData wit_get_data();	//獲取數據
+	void wit_flush();	//清除數據
 };
 
 #endif
