d0c32d6b3aba9d0a0ed28558e7399ed731f1061f
NH3andH2O <accord_flytrap6x@icloud.com>
2025-04-27 22:31:14 +0800

    伺服電機限位獨立變數

--- src/eyesMove.cpp
+++ src/eyesMove.cpp
@@ -20,31 +20,34 @@ void eyesMove::eyesMove_init()
 
 void eyesMove::eyesMove_servo(uint8_t upper_eyelid_angle, uint8_t lower_eyelid_angle, uint8_t eyeball_angle)
 {
+
 	/* 角度輸入超限更正 */
-	if(upper_eyelid_angle < 100)
+	if(upper_eyelid_angle < UPPER_EYELID_ANGLE_MIN)
 	{
-	upper_eyelid_angle = 100;
+	upper_eyelid_angle = UPPER_EYELID_ANGLE_MIN;
 	}
-	else if(upper_eyelid_angle > 175)
+	else if(upper_eyelid_angle > UPPER_EYELID_ANGLE_MAX)
 	{
-		upper_eyelid_angle = 175;
+		upper_eyelid_angle = UPPER_EYELID_ANGLE_MAX;
 	}
-	if(lower_eyelid_angle < 35)
+	if(lower_eyelid_angle < LOWER_EYELID_ANGLE_MIN)
 	{
-		lower_eyelid_angle = 35;
+		lower_eyelid_angle = LOWER_EYELID_ANGLE_MIN;
 	}
-	else if(lower_eyelid_angle > 90)
+	else if(lower_eyelid_angle > LOWER_EYELID_ANGLE_MAX)
 	{
-		lower_eyelid_angle = 90;
+		lower_eyelid_angle = LOWER_EYELID_ANGLE_MAX;
 	}
-	if(eyeball_angle < 55)
+	if(eyeball_angle < EYEBALL_ANGLE_MIN)
 	{
-		eyeball_angle = 55;
+		eyeball_angle = EYEBALL_ANGLE_MIN;
 	}
-	else if(eyeball_angle > 125)
+	else if(eyeball_angle > EYEBALL_ANGLE_MAX)
 	{
-		eyeball_angle = 125;
+		eyeball_angle = EYEBALL_ANGLE_MAX;
 	}
+
+	/* 眼皮輸出 */
 	upper_eyelid_servo.write(upper_eyelid_angle);
 	lower_eyelid_servo.write(lower_eyelid_angle);
 	eyeball_servo.write(eyeball_angle);
@@ -57,19 +60,19 @@ void eyesMove::eyesMove_angle(int8_t eyelid_angle, int8_t x_angle, int8_t y_angl
 	{
 		eyelid_angle = 0;
 	}
-	else if(eyelid_angle > 75)
+	else if(eyelid_angle > 80)
 	{
-		eyelid_angle = 75;
+		eyelid_angle = 80;
 	}
 	
 	/* y輸入超限更正 */
-	if(eyelid_angle <= 37 && abs(y_angle) > eyelid_angle)
+	if(eyelid_angle <= 80 && abs(y_angle) > eyelid_angle)
 	{
 		y_angle = (y_angle > 0) ? eyelid_angle : -eyelid_angle;
 	}
-	else if(eyelid_angle > 37 && abs(y_angle) > abs(eyelid_angle - 75))
+	else if(eyelid_angle > 80 && abs(y_angle) > abs(eyelid_angle - 80))
 	{
-		y_angle = (y_angle > 0) ? abs(eyelid_angle - 75) : -abs(eyelid_angle - 75);
+		y_angle = (y_angle > 0) ? abs(eyelid_angle - 80) : -abs(eyelid_angle - 80);
 	}
 
 	/* x輸入超限更正 */
@@ -83,9 +86,9 @@ void eyesMove::eyesMove_angle(int8_t eyelid_angle, int8_t x_angle, int8_t y_angl
 	}
 
 	/* 眼皮輸出角度确定 */
-	uint8_t upper_eyelid_angle = 175 - eyelid_angle - y_angle;						//上眼皮角度
-	uint8_t lower_eyelid_angle = 35 + eyelid_angle - map(y_angle, 0, 75, 0, 55);	//下眼皮角度
-	uint8_t eyeball_angle = 90 + x_angle;											//眼球角度
+	uint8_t upper_eyelid_angle = UPPER_EYELID_ANGLE_MAX - eyelid_angle - map(y_angle, 0, 80, 0, UPPER_EYELID_ANGLE_MAX - UPPER_EYELID_ANGLE_MIN);	//上眼皮角度
+	uint8_t lower_eyelid_angle = LOWER_EYELID_ANGLE_MIN + eyelid_angle - map(y_angle, 0, 80, 0, LOWER_EYELID_ANGLE_MAX - LOWER_EYELID_ANGLE_MIN);	//下眼皮角度
+	uint8_t eyeball_angle = (EYEBALL_ANGLE_MAX + EYEBALL_ANGLE_MIN) / 2 + x_angle;											//眼球角度
 
 	/* 眼皮輸出 */
 	eyesMove_servo(upper_eyelid_angle, lower_eyelid_angle, eyeball_angle);	//設置眼皮角度
--- src/eyesMove.h
+++ src/eyesMove.h
@@ -6,9 +6,18 @@
 class eyesMove
 {
 private:
-	Servo upper_eyelid_servo;	//上眼皮伺服馬達限位：100°-180°
-	Servo lower_eyelid_servo;	//下眼皮伺服馬達限位：55°-115°
-	Servo eyeball_servo;		//眼球伺服馬達限位：55°-125°
+
+	/* 伺服馬達限位 */
+	const uint8_t UPPER_EYELID_ANGLE_MAX = 175;	//上眼皮最大角度
+	const uint8_t UPPER_EYELID_ANGLE_MIN = 100;	//上眼皮最小角度
+	const uint8_t LOWER_EYELID_ANGLE_MAX = 90;	//下眼皮最大角度
+	const uint8_t LOWER_EYELID_ANGLE_MIN = 35;	//下眼皮最小角度
+	const uint8_t EYEBALL_ANGLE_MAX = 125;		//眼球最大角度
+	const uint8_t EYEBALL_ANGLE_MIN = 55;		//眼球最小角度
+
+	Servo upper_eyelid_servo;	//上眼皮伺服馬達
+	Servo lower_eyelid_servo;	//下眼皮伺服馬達
+	Servo eyeball_servo;		//眼球伺服馬達
 public:
 	/* 伺服馬達引脚 */
 	uint8_t upper_eyelid_pin;
