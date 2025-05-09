e3daecdc4b0bdc47f5299de79ff571e748f46e1a
NH3andH2O <accord_flytrap6x@icloud.com>
2025-05-09 17:37:09 +0800

    優化眼鏡更新邏輯
    將上次更新次數改為記錄上次更新時間

--- src/gc9a01.cpp
+++ src/gc9a01.cpp
@@ -62,7 +62,7 @@ void GC9A01::GC9A01_init()
 	this->myLGFX.setBrightness(255);	//設置亮度
 	this->myLGFX.init();				//初始化
 
-	this->mySprite.setColorDepth(16);			//設置顏色深度
+	this->mySprite.setColorDepth(16);	//設置顏色深度
 	this->mySprite.createSprite(240, 240);
 
 	this->myLGFX.fillScreen(this->myLGFX.color565(0, 0, 0));
@@ -70,10 +70,15 @@ void GC9A01::GC9A01_init()
 
 void GC9A01::GC9A01_setEyes_r(uint8_t target_eyesR, double zeta = -1.0, double omega_n = -1.0)
 {
-	if(target_eyesR != this->target_eyesR && this->lastChange_eyesR >= lastChangeMax) 
+	if(target_eyesR != this->target_eyesR && this->lastChange_eyesR >= LASTCHANGEMAX) 
 	{
 	this->lastUpdate_eyesR = xTaskGetTickCount();	//設置眼睛半徑更新時間
-	this->lastChange_eyesR = 0;						//設置眼睛半徑變化次數
+	this->lastChange_eyesR = xTaskGetTickCount();	//設置眼睛半徑變化次數
+	this->isUpdate_eyesR = 1;						//設置眼睛更新狀態
+	} 
+	else
+	{
+		this->isUpdate_eyesR = 0;					//設置眼睛更新狀態
 	} 
 	this->target_eyesR = target_eyesR;				//設置目標眼睛半徑
 	if(zeta != -1.0) 
@@ -88,10 +93,11 @@ void GC9A01::GC9A01_setEyes_r(uint8_t target_eyesR, double zeta = -1.0, double o
 
 void GC9A01::GC9A01_setEyes_lightMax(uint8_t lightMax, double zeta = -1.0, double omega_n = -1.0)
 {
-	if(lightMax != this->target_lightMax && this->lastChange_lightMax >= lastChangeMax) 
+	if(lightMax != this->target_lightMax && this->lastChange_lightMax >= LASTCHANGEMAX) 
 	{
 		this->lastUpdate_lightMax = xTaskGetTickCount();	//設置光暈更新時間
-		this->lastChange_lightMax = 0;						//設置光暈變化次數
+		this->lastChange_lightMax = xTaskGetTickCount();	//設置光暈變化次數
+		this->isUpdate_lightMax = 1;						//設置光暈更新狀態
 	} 
 	this->target_lightMax = lightMax;						//設置目標光暈最大值
 	if(zeta != -1.0) 
@@ -109,23 +115,31 @@ uint8_t GC9A01::GC9A01_update()
 	this->mySprite.fillScreen(this->myLGFX.color565(0, 0, 0));	//清屏
 
 	/* 控制眼睛动态效果 */
-	if(this->lastChange_eyesR < lastChangeMax)
+	if(this->isUpdate_eyesR)
 	{
 		uint64_t now = xTaskGetTickCount();						//獲取當前時間
 		double dt = (now - this->lastUpdate_eyesR) / 1000.0;	//計算時間差
 		if(dt > 0.001)
 		{
 			this->lastUpdate_eyesR = now;	//更新時間
+
+			/* 二階系統控制 */
 			double acceleration = -2 * this->zeta_eyesR * this->omega_n_eyesR * this->deyesR - this->omega_n_eyesR * this->omega_n_eyesR * (this->eyesR - this->target_eyesR);
 			this->deyesR += acceleration * dt;
 			this->eyesR += this->deyesR * dt;
-			if((uint8_t)round(this->eyesR) == (uint8_t)round(this->target_eyesR))	//如果眼睛半徑達到目標值
+
+			/* 檢查更新狀態 */
+			if((uint8_t)round(this->eyesR) == (uint8_t)this->target_eyesR)	//如果眼睛半徑達到目標值
 			{
-				this->lastChange_eyesR++;	//增加變化次數
+				;
 			}
 			else
 			{
-				this->lastChange_eyesR = 0;	//重置變化次數
+				this->lastChange_eyesR = now;
+			}
+			if(this->lastChange_eyesR - now > (uint64_t)this->LASTCHANGEMAX)	//如果時間差大於LASTCHANGEMAX
+			{
+				this->isUpdate_eyesR = 0;	//設置更新狀態為0
 			}
 		}
 
@@ -134,23 +148,31 @@ uint8_t GC9A01::GC9A01_update()
 	}
 
 	/* 控制光暈動態效果 */
-	if(this->lastChange_lightMax < lastChangeMax)
+	if(this->isUpdate_lightMax)
 	{
 		uint64_t now = xTaskGetTickCount();		//獲取當前時間
 		double dt = (now - this->lastUpdate_lightMax) / 1000.0;	//計算時間差
 		if(dt > 0.001)
 		{
 			this->lastUpdate_lightMax = now;	//更新時間
+
+			/* 二階系統控制 */
 			double acceleration = -2 * this->zeta_lightMax * this->omega_n_lightMax * this->dlightMax - this->omega_n_lightMax * this->omega_n_lightMax * (this->lightMax - this->target_lightMax);
 			this->dlightMax += acceleration * dt;
 			this->lightMax += this->dlightMax * dt;
-			if((uint8_t)round(this->lightMax) == (uint8_t)round(this->target_lightMax))	//如果光暈達到目標值
+
+			/* 檢查更新狀態 */
+			if((uint16_t)round(this->lightMax) == (uint16_t)this->target_lightMax)	//如果光暈達到目標值
 			{
-				this->lastChange_lightMax++;	//增加變化次數
+				;
 			}
 			else
 			{
-				this->lastChange_lightMax = 0;	//重置變化次數
+				this->lastChange_lightMax = now;
+			}
+			if(this->lastChange_lightMax - now > (uint64_t)this->LASTCHANGEMAX)	//如果時間差大於LASTCHANGEMAX
+			{
+				this->isUpdate_lightMax = 0;	//設置更新狀態為0
 			}
 		}
 
@@ -172,14 +194,15 @@ uint8_t GC9A01::GC9A01_update()
 	this->mySprite.pushSprite(&this->myLGFX, 0, 0);	//推送
 
 	/* 返回是否更新完成 */
-	if(this->lastChange_eyesR < this->lastChangeMax || this->lastChange_lightMax < this->lastChangeMax)	//如果沒達到目標值
-	{
-		return 0;	//未完成
-	}
-	else
+	if(this->isUpdate_eyesR == 0 && this->isUpdate_lightMax == 0)	//如果眼睛半徑和光暈都沒有更新
 	{
 		return 1;	//完成
 	}
+	else
+	{
+		return 0;	//未完成
+	}
+
 }
 
 GC9A01GetData GC9A01::GC9A01_get_data()
