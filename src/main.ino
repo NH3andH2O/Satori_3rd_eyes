2322b2d6fd8726b45a6499edc0d405a4a85ff63c
NH3andH2O <accord_flytrap6x@icloud.com>
2025-06-02 11:43:48 +0800

    專案發佈

--- src/main.ino
+++ src/main.ino
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define VERSION "1.0.0"
+
 #include <ESP32Servo.h>
 #include <LovyanGFX.hpp>
 #include "gc9a01.h"
