0201964f68b8951d7866c623d6468b66d0eee90e
NH3andH2O <accord_flytrap6x@icloud.com>
2025-10-26 00:25:44 +0800

    版本發佈
    v1.2.0

--- platformIO/src/main.cpp
+++ platformIO/src/main.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define VERSION "1.1.0"
+#define VERSION "1.2.0"
 
 /* 第三方程式庫 */
 #include <Arduino.h>
